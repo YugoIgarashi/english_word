9d97576af137b23d5f43c192e42aac15c0711e63
LazyGorilla <yg0805.bdjm@outlook.com>
2021-07-18 15:05:20 +0900

    add vue,js

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root "posts#index"
+  get '/posts' ,to: "posts#index"
+  get "/posts/:id",to:"posts#show"
+  get '/' => "home#top"
+  get '/about' => "home#about"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
