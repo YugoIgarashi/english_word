f2f7c8cddcd9bda48a39fe9c7832b0e2255ba8ac
LazyGorilla <yg0805.bdjm@outlook.com>
2021-07-22 13:33:54 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  #get 'users/new'
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
-  resources :microposts
   resources :users
-  get 'page/index'
+  resources :microposts
 
  
-  get '/posts' ,to: "posts#index"
-  get "/posts/:id",to:"posts#show"
-  get '/' => "home#top"
-  get '/about' => "home#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
