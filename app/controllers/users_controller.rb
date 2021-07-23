529a30b16a23a1d009af65325ed85d770b2e68d8
LazyGorilla <yg0805.bdjm@outlook.com>
2021-07-23 14:31:15 +0900

    Finish layout and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params) 
+    @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -19,9 +20,9 @@ class UsersController < ApplicationController
   end
 
   private
+
     def user_params
-      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
-
-
 end
