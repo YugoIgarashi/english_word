02d963b746e9e741d13acd9bd8d961b850ef7241
LazyGorilla <yg0805.bdjm@outlook.com>
2021-07-25 15:55:48 +0900

    Finish layout and routes 7/25

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,24 +1,37 @@
 module SessionsHelper
-    # 渡されたユーザーでログインする
-    def log_in(user)
-        session[:user_id] = user.id
-    end
+  # 渡されたユーザーでログインする
+  def log_in(user)
+      session[:user_id] = user.id
+  end
     
-    # 現在ログイン中のユーザーを返す（いる場合）
-    def current_user
-        if session[:user_id]
-            @current_user ||= User.find_by(id:session[:user_id])
-        end
-    end
-    
-    # ユーザーがログインしていればtrue、その他ならfalseを返す
-    def logged_in?
-        !current_user.nil?
-    end
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
 
-    def log_out
+  # 現在ログイン中のユーザーを返す（いる場合）
+  def current_user
+      if session[:user_id]
+          @current_user ||= User.find_by(id:session[:user_id])
+      elsif cookies.signed[:user_id]
+        user = User.find_by(id:cookies.signed[:user_id])
+        if user && user.authenticated?(cookies[:remember_token])
+            log_in user@current_user = user
+        end
+        
+      end
+  end
+    
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+      !current_user.nil?
+  end
+
+  def log_out
     session.delete(:user_id)
     @current_user = nil
-    end
+  end
 
 end
