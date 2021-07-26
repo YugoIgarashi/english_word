fde769d609732f85ea44174e734d597cb0cf75fc
LazyGorilla <yg0805.bdjm@outlook.com>
2021-07-26 16:23:41 +0900

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,8 @@ Rails.application.configure do
   host = 'pure-refuge-76680.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :port           => 587,
+    :address        => smtp.mailgun.org,
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain         => host,
